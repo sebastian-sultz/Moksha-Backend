784befb988e8dd75c245137b5463173892963060
sebastian-sultz <hiadarshtripathi@gmail.com>
2025-02-03 18:17:19 +0530

    final

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 const AuthRouter = require('./Routes/AuthRouter');
 const ProductRouter = require('./Routes/ProductRouter');
 const musicRoutes = require('./Routes/musicRoutes');
+const quizRoutes =require('./Routes/quizRoutes.js');
+
 // const path = require("path");
 
 require('dotenv').config();
@@ -24,6 +26,8 @@ app.use(cors());
 app.use('/auth', AuthRouter);
 app.use('/products', ProductRouter);
 app.use('/api/music', musicRoutes);
+app.use('/api/quiz', quizRoutes);
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
